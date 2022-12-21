81d51de939e3f07511bb14f7637ac080387e7683
aitolkun <aitolkuns52@gmail.com>
2022-12-21 15:16:16 +0600

    account

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.User'
 
 
 from datetime import timedelta
