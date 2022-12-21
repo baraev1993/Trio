6fdf03da519b7d409f5663e8fe175af1fe552b61
baraev1993 <baraev1993@gmail.com>
2022-12-21 21:53:35 +0600

    acti code tokken, view

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+swagger_view = get_schema_view(
+    openapi.Info(
+        title="Blog API",
+        default_version = 'v1',
+        description = "blog API",
+    ),
+    public=True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', swagger_view.with_ui('swagger', cache_timeout=0)),
+    path('account/', include('account.urls')),
+    path('',include('main.urls')),
 ]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -1,10 +1,11 @@
+from django.shortcuts import render
 from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=70)
-    def str(self):
-        return self.title
-
+    
+    def __str__(self):
+        return f'{self.title}'
 
 class Film(models.Model):
     name = models.CharField(max_length=50)
@@ -14,3 +15,23 @@ class Film(models.Model):
     runtime = models.PositiveIntegerField()
     cast = models.TextField()
     category =models.ForeignKey(Category, related_name='film', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Comment(models.Model):
+    user = models.IntegerField()
+    body = models.TextField()
+    craeted_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+class User(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=10)
+    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)        
+
+class Rating(models.Model):
+    user = models.IntegerField()
+    film = models.IntegerField()
+    value = models.IntegerField()
