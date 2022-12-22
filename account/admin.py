3a221f3af2bbdbc30d28efe8cc11e22e6d0762d0
baraev1993 <baraev1993@gmail.com>
2022-12-22 18:00:30 +0600

    review permission

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from main.models import Category,Film
+# from main.models import Category,Film
 from .models import User
 
 admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Film)
+
--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.viewsets import generics
 
-from rest_framework.viewsets import ModelViewSet
-
+from .serializers import CategorySerializer, FilmSerializer
 from .models import Category, Film
 
-from .serializers import CategorySerializer, FilmSerializer
+
+
 
 class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
@@ -30,3 +36,32 @@ class CategoryListView(generics.ListAPIView):
 class FilmViewSet(ModelViewSet):
     queryset = Film.objects.all()
     serializer_class= FilmSerializer
+
+    def get_permissions(self):
+        if self.action in ['retrieve', 'list', 'search']:
+            # если это запрос на листинг или детализацию
+            return [] # разрешаем всем
+        return [IsAdminUser()] # разрешаем только админам
+
+    @swagger_auto_schema(manual_parameters=[
+    openapi.Parameter('q', openapi.IN_QUERY, type=openapi.TYPE_STRING)
+    ])
+    @action(['GET'], detail=False)
+    def search(self, request):
+        # /products/search/?q=hello
+        # query_params = {'q':'hello'}
+        q = request.query_params.get('q')
+        queryset = self.get_queryset() # Product.objects.all()
+        if q:
+            # queryset = queryset.filter(title__icontains=q) # title ilike '%hello%'
+            queryset = queryset.filter(Q(name__icontains=q) | Q(description__icontains=q))
+            # title ilike '%hello%' or description ilike '%hello%'
+        # get_serializer - ProductSerializer
+
+        pagination = self.paginate_queryset(queryset)
+        if pagination:
+            serializer = self.get_serializer(pagination, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
