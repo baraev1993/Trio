0de4b38589ff63b493d4351532f2d9e8a430118b
baraev1993 <baraev1993@gmail.com>
2022-12-22 12:19:30 +0600

    .

--- main/models.py
+++ main/models.py
@@ -11,7 +11,7 @@ class Film(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     year = models.PositiveIntegerField()
-    poster = models.ImageField(upload_to='film', null=True)
+    poster = models.ImageField(upload_to='films', null=True)
     runtime = models.PositiveIntegerField()
     cast = models.TextField()
     category =models.ForeignKey(Category, related_name='film', on_delete=models.CASCADE)
