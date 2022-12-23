9f101c6fb3ce9aed095b193d6acd335c647c076b
baraev1993 <baraev1993@gmail.com>
2022-12-23 16:54:51 +0600

    .

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
     'main',
     'account',
-    #'review',
+    'review',
 
 ]
 
--- main/views.py
+++ main/views.py
@@ -17,21 +17,6 @@ class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-# class FilmView(generics.ListCreateAPIView):
-#     queryset = Film.objects.all()
-#     serializer_class = FilmSerializer
-
-# class FilmDetailView(generics.RetrieveAPIView):
-#     queryset = Film.objects.all()
-#     serializer_class = FilmSerializer
-
-# class FilmUpdateView(generics.UpdateAPIView):
-#     queryset = Film.objects.all()
-#     serializer_class = FilmSerializer
-
-# class FilmDeleteView(generics.DestroyAPIView):
-#     queryset = Film.objects.all()
-#     serializer_class = FilmSerializer
 
 class FilmViewSet(ModelViewSet):
     queryset = Film.objects.all()
